4d52cd9ad86c5f4ebf090dc8a267887c06f0700b
Akritishukla <akriti14shukla@gmail.com>
2024-01-16 04:48:35 +0530

    Add files via upload

---
+++ 01_functions.js
@@ -0,0 +1,23 @@
+
+function sayMyName(){
+console.log("A")
+console.log("K")
+console.log("R")
+console.log("I")
+console.log("T")
+console.log("I")
+}
+//sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2)
+// }
+
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2)
+}
+
+
+const result = addTwoNumbers(3,5)
+console.log(result);
